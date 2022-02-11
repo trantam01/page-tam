b8c0cd88229d115c5312b633ce15838b2c9312e8
tran van tam <gotentam01@gmail.com>
2022-02-11 11:52:59 +0800

    all page-tam

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Page://tam"/>
+
+    <header-app/>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    HeaderApp: Header
+   
+    
+  },
+};
+</script>
+
+<style>
+img{
+  height: 200px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <!-- <h3>My company </h3> -->
+     <p>
+      I work here<br />
+      <a href="https://eip.ypcloud.com/zh_TW/" target="_blank" rel="noopener"
+        >YPCLOUD</a
+      >.
+    </p>
+
+    <ul>
+    <li>
+      <img alt="chibi" src="../assets/jboard.png">
+     <a  
+
+          href="https://jboard.ypcloud.com/"
+          target="_blank"
+          rel="noopener"
+          >jboard</a>
+
+  <img alt="chibi" src="../assets/jbuilder.png">
+     <a  
+
+          href="https://jbuilder.ypcloud.com/"
+          target="_blank"
+          rel="noopener"
+          >jbuilder</a>     
+
+<img alt="chibi" src="../assets/fbuilder.png">
+     <a  
+
+          href="https://fbuilder.ypcloud.com/"
+          target="_blank"
+          rel="noopener"
+          >jbuilder</a>
+             
+    </li>
+    </ul>
+    <ul>
+
+<img alt="chibi" src="../assets/ioctouch.png">
+     <a  
+          href="https://ioctouch.ypcloud.com/"
+          target="_blank"
+          rel="noopener"
+          >ioctouch</a>
+        
+
+<img alt="chibi" src="../assets/gitwork.png">
+     <a  
+          href="https://gitwork.ypcloud.com/"
+          target="_blank"
+          rel="noopener"
+          >gitwork</a>
+        
+
+<img alt="chibi" src="../assets/tg.png">
+     <a  
+          href="https://web.telegram.org/"
+          target="_blank"
+          rel="noopener"
+          >ioctouch</a>
+        
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  
+}
+img{
+  height: 100px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
